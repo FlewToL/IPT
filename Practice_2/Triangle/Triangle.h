60e3f09faef993c50fe955e59ecf1316c9c9e75d
FlewToL <112846459+FlewToL@users.noreply.github.com>
2023-02-23 22:53:31 +0300

    Add files via upload

---
+++ Practice_2/Triangle/Triangle.h
@@ -0,0 +1,13 @@
+#pragma once
+class Triangle
+{
+private:
+    double a, b, c;
+
+public:
+    bool exst_tr();
+    void set(double a1, double b1, double c1);
+    void show();
+    double perimetr();
+    double square();
+};
---
+++ Practice_2/Triangle/main.cpp
@@ -0,0 +1,27 @@
+#include "Triangle.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    Triangle mas[3];
+    double a, b, c;
+    for (int i = 0; i < 3; i++) {
+        cout << "Введите a, b и c для треугольника № " << i + 1 << " через пробел: " << endl;
+        cin >> a >> b >> c;
+        mas[i].set(a, b, c);
+        if (!(mas[i].exst_tr())) {
+            mas[i].show();
+            cout << "Треугольника с такими сторонами не существует, попробуйте ещё раз" << endl;
+            i--;
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        mas[i].show();
+        cout << "Периметр треугольника: " << mas[i].perimetr() << endl;
+        cout << "Площадь треугольника: " << mas[i].square() << endl;
+    }
+    return 0;
+}
